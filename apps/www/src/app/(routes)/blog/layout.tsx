73dccbfa4703e365c2f264e07a49fe20bb752956
Omid Shabab <omidshababdev@gmail.com>
2024-04-28 09:20:39 -0400

    0.1.0

---
+++ apps/www/src/app/(routes)/blog/layout.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react"
+
+export default function layout({
+     children
+}: {
+     children: ReactNode,
+}) {
+     return (
+          <div>
+               {children}
+          </div>
+     )
+}
