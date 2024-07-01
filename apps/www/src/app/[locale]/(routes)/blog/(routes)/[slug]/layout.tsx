d06665d2fb0b64b4a2c5ec059b7cfa77fe0d1bdb
Omid Shabab <omidshababdev@gmail.com>
2024-07-01 14:42:29 -0400

    0.1.0

--- apps/www/src/app/[locale]/(routes)/blog/(routes)/[slug]/layout.tsx
+++ apps/www/src/app/[locale]/(routes)/blog/(routes)/[slug]/layout.tsx
@@ -2,26 +2,26 @@ import { Post } from "@/types";
 import axios from "axios";
 import { Metadata } from "next";
 
-export const generateMetadata = async ({ params }: { params: { slug: string } }): Promise<Metadata> => {
-     const res = await axios.get(`${process.env.API_BASE_URL}/posts/${params.slug}`);
-     const post: Post = res.data;
+// export const generateMetadata = async ({ params }: { params: { slug: string } }): Promise<Metadata> => {
+//      const res = await axios.get(`${process.env.API_BASE_URL}/posts/${params.slug}`);
+//      const post: Post = res.data;
 
-     return {
-          title: post.title.toLowerCase(),
-          description: JSON.parse(post.desc).content[0]?.content[0]?.text || 'Post description',
-     };
-};
+//      return {
+//           title: post.title.toLowerCase(),
+//           description: JSON.parse(post.desc).content[0]?.content[0]?.text || 'Post description',
+//      };
+// };
 
-export async function generateStaticParams() {
-     const res = await axios.get(`${process.env.API_BASE_URL}/posts`);
-     const posts: Post[] = res.data;
+// export async function generateStaticParams() {
+//      const res = await axios.get(`${process.env.API_BASE_URL}/posts`);
+//      const posts: Post[] = res.data;
 
-     return posts.map(post => ({
-          params: {
-               slug: post.slug
-          }
-     }));
-}
+//      return posts.map(post => ({
+//           params: {
+//                slug: post.slug
+//           }
+//      }));
+// }
 
 export default function layout({
      children
