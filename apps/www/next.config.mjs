110450fdda008efa855a4a1815938059c2dba52f
Omid Shabab <omidshababdev@gmail.com>
2024-07-02 10:54:58 -0400

    0.1.0

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -14,11 +14,6 @@ const nextConfig = {
       },
     ],
   },
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-    GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-    SITE_URL: process.env.SITE_URL,
-  },
 };
 
 export default withNextIntl(nextConfig);
