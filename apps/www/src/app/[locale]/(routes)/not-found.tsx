5772830bc2835ef067fcc231e5f8c1cc3513f36a
Omid Shabab <omidshababdev@gmail.com>
2024-09-26 16:14:34 +0330

    0.1.0

--- apps/www/src/app/[locale]/(routes)/not-found.tsx
+++ apps/www/src/app/[locale]/(routes)/not-found.tsx
@@ -1,23 +1,26 @@
 "use client"
 
 import { Button } from '@repo/ui/components/button'
+import { useTranslations } from 'next-intl'
 import { useRouter } from 'next/navigation'
 
 export default function NotFound() {
      const router = useRouter()
 
+     const tNotFound = useTranslations("not_found")
+
      return (
-          <div className="flex flex-col w-full h-screen justify-center items-center gap-y-[35px]">
+          <div className="flex flex-col w-full min-h-[650px] justify-center items-center gap-y-[35px]">
                <div className="flex flex-col gap-y-[10px] text-center">
-                    <h2 className="text-[25px] font-bold text-slate-800">Not Found what you looking for!</h2>
-                    <p className="text-[15px] text-slate-600">Try it again or contact the support to help you.</p>
+                    <h2 className="text-[25px] font-bold text-slate-800">{tNotFound("title")}</h2>
+                    <p className="text-[15px] text-slate-600">{tNotFound("desc")}</p>
                </div>
                <Button
                     variant="secondary"
                     size="sm"
                     className="px-[28px] py-[25px] rounded-[12px]"
                     onClick={() => router.push("/")}>
-                    Return Home
+                    {tNotFound("button")}
                </Button>
           </div>
      )
